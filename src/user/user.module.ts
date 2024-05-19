febb2d7860908e2b4b128e78f8f8f41871068e10
Nikola Petrovic <s.nino.petrovic@gmail.com>
2024-05-19 10:13:09 +0200

    feat: user delete endpoint and logic

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { User } from './entities/user.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { User } from "./entities/user.entity";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "RESERVATION_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          port: 1315,
+        },
+      },
+    ]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Inject, Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./entities/user.entity";
+import { ClientProxy, RpcException } from "@nestjs/microservices";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @Inject("RESERVATION_SERVICE") private readonly reservationClient:
+      ClientProxy,
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+  async create(dto: CreateUserDto) {
+    const user = this.usersRepository.create(dto);
+    return this.usersRepository.save(user);
   }
 
   findAll() {
@@ -21,6 +31,11 @@ export class UserService {
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    if (this.reservationClient.send<boolean>("hasActiveReservations", id)) {
+      throw new RpcException(
+        "User cannot be deleted because he has active reservations in future",
+      );
+    }
+    return this.usersRepository.delete(id);
   }
 }
