924e122f70e3b7a0e40d146ac14004fc8e316f23
Nikola Petrovic <40899687+nikolapetrovic1@users.noreply.github.com>
2024-09-04 12:41:21 +0200

    feat: user-credentials (#6)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { MessagePattern, Payload } from "@nestjs/microservices";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { UpdateCredentialsDto } from "./dto/update-credentials.dto";
 
 @Controller()
 export class UserController {
@@ -31,6 +32,11 @@ export class UserController {
   @MessagePattern("updateUser")
   update(@Payload() updateUserDto: UpdateUserDto) {
     return this.userService.update(updateUserDto.id, updateUserDto);
+  }
+
+  @MessagePattern("updateCredentials")
+  updateCredentials(@Payload() updateUserDto: UpdateCredentialsDto) {
+    return this.userService.updateCredentials(updateUserDto);
   }
 
   @MessagePattern("removeUser")
