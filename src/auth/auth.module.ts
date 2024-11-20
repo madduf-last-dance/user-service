149e06cd5f878da1e8ea1aefbcebe260dd65115e
Nikola Petrovic <40899687+nikolapetrovic1@users.noreply.github.com>
2024-11-20 14:34:15 +0100

    feat: update hosts for client module register

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,6 +19,7 @@ import { ClientsModule, Transport } from "@nestjs/microservices";
         name: "RESERVATION_SERVICE",
         transport: Transport.TCP,
         options: {
+          host: 'reservation-service.default.svc.cluster.local',
           port: 1315,
         },
       },
@@ -26,7 +27,7 @@ import { ClientsModule, Transport } from "@nestjs/microservices";
         name: "ACCOMMODATION_SERVICE",
         transport: Transport.TCP,
         options: {
-          host: "accommodation-service",
+          host: 'accommodation-service.default.svc.cluster.local',
           port: 1312,
         },
       },
